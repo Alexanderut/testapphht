6abc47839eedabf86c23d31f6600755c4139a138
alexanderut <alexander.utne@gmail.com>
2024-02-19 12:23:22 +0100

    første commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import bilde from "./bilde.jpg"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <img src={bilde} className="Alex-logo" alt="bare et bilde av alex"/>
+        
+        
+        <h1>Dette er BED-3101</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.uit.no"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React er så kult. kom å se på uit sin nettside
         </a>
       </header>
     </div>
